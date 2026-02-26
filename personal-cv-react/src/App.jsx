62095706638601a1ddebf5f002d4b66d040cae93
BellaDeens <archelleaparici@gmail.com>
2026-02-26 18:59:17 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/App.jsx
+++ personal-cv-react/src/App.jsx
@@ -5,21 +5,25 @@ import Education from "./components/Education";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { useState } from "react";
+
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  
   return (
     <div className={darkMode ? "dark-mode" : ""}>
-      <button onClick={() => setDarkMode(!darkMode)}>
-      Toggle Dark Mode
-      </button>
-      <Header />
-      <About />
-      <Skills />
-      <Education />
-      <Contact /> 
-      <Footer/>
-      <script src="js/script.js"></script>
+      <div className="container">
+        <button onClick={() => setDarkMode(!darkMode)}>
+        Toggle Dark Mode
+        </button>
+        <Header />
+        <About />
+        <Skills />
+        <Education />
+        <Contact /> 
+        <Footer/>
+        <script src="js/script.js"></script>
       </div>
+    </div>
    
   );
 }
